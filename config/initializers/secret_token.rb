f83555ac81cde4ae6e0537d6f6edf95cfe11e08e
norinori2222 <norinori2222@abelia.ocn.ne.jp>
2012-03-24 14:38:55 +0900

    'first'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KojimachiBlog::Application.config.secret_token = '6f3cdf78fc7a451ff2165e465c85bab3eda597be863f892092ffb0aa68e02c368de76972826657548ae68b3f0f3e137e4a3a616455af9de5ab19195d9f6f330c'
